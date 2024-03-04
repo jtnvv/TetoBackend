b19473b4d9c7cf1f1141968b0b619c1a55c02ea2
daperezch <daperezch@unal.edu.co>
2024-03-04 07:14:15 -0500

    db ignore

--- database.sql
+++
@@ -1,7 +0,0 @@
---users table
-create table users(
-    user_id serial primary key,
-    email varchar(255) unique not null,
-    password varchar(255) unique not null,
-    created_at date default current_date
-);

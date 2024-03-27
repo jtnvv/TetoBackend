10b98f18ab4d797c800ad3e9c8a14d152ab34c1f
Juan Acevedo <juan.acevedo9805@gmail.com>
2024-03-26 23:33:52 -0500

    26 - Items register by logged store and store singup

--- src/database/models/Store.js
+++ src/database/models/Store.js
@@ -1,8 +1,23 @@
 import { Model, DataTypes } from "sequelize";
 import sequelize from "./index.js";
 import Item from "./Item.js";
+import User from "./User.js";
 
 const Store = class extends Model {
+  static associate(models) {
+    Store.hasMany(Item, {
+      foreignKey: "store_id",
+      sourceKey: "id",
+    });
+    Item.belongsTo(Store, {
+      foreignKey: {
+        name: 'store_id',
+      },
+    });
+    Item.belongsToMany(User, {
+      through: "Favorites",
+    });
+  }
 };
 
 await Store.init(
@@ -14,11 +29,14 @@ await Store.init(
     phone_number: DataTypes.STRING,
     description: DataTypes.STRING,
     logo: DataTypes.STRING,
+    role: DataTypes.STRING,
   },
   {
     sequelize,
     modelName: "store",
   }
 );
+
+Store.associate();
 
 export default Store;
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,19 +3,18 @@ import {
   getUsers,
   register,
   login,
-  protectedRoute,
   logout,
+  brandLogin,
 } from "../controllers/auth.controller.js";
 import { validationMiddleware } from "../middlewares/validations-middleware.js";
-import { registerValidation, loginValidation } from "../validators/auth.js";
-import {userAuth} from '../middlewares/auth-middleware.js';
+import { registerValidation, loginValidation, loginStoreValidation } from "../validators/auth.js";
 
 const router=Router();
 
 router.get('/get-users',getUsers);
-router.get("/protected", userAuth({ability: "hola"}), protectedRoute);
 router.post('/register', registerValidation, validationMiddleware, register);
-router.post('/login', loginValidation, validationMiddleware,login);
+router.post('/login', loginValidation, validationMiddleware, login);
+router.post('/login-store', loginStoreValidation, brandLogin);
 router.get('/logout',logout);
 
 export default router;

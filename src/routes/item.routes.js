d9986a1b1693e5cb83c2f3bb0c759878ac1f37e2
jtnvv <jonathan.jnthnd@gmail.com>
2024-04-20 00:50:42 -0500

    40- Realizar consultas de los productos disponibles

--- src/routes/item.routes.js
+++ src/routes/item.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { userAuth } from '../middlewares/auth-middleware.js';
-import { createItem, getItemsByStore, getCategories, getItemsByCategory, getColors, getSizes, getItemsById } from "../controllers/item.controller.js";
+import { createItem, getItemsByStore, getCategories, getItemsByCategory, getColors, getSizes, getItemsById, getItemsByPriority } from "../controllers/item.controller.js";
 const router = Router();
 
 router.post("/store-item", userAuth({ ability: "store_item" }), createItem);
@@ -10,5 +10,6 @@ router.get("/get-colors", getColors);
 router.get("/get-sizes", getSizes);
 router.get("/get-items-by-category/:category", getItemsByCategory);
 router.get("/product/:product_id", getItemsById);
+router.get("/get-items-by-priority", getItemsByPriority);
 
 export default router;

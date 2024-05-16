5cc0cfa43ffc4ded7f16d88b53994a7609a892bd
SergioACV <secastrov@unal.edu.co>
2024-05-16 17:29:28 -0500

    Consultas para borrar ordenes y enviar correos de cancelacion

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { userAuth } from "../middlewares/auth-middleware.js";
-import { createPaymentLink, fetchBrandOrders, fetchUserOrders, itemRating, updateOrderRating } from "../controllers/order.controller.js";
+import { createPaymentLink, deleteOrder, fetchBrandOrders, fetchUserOrders, itemRating, sendEmailCancelOrder, updateOrderRating } from "../controllers/order.controller.js";
+
 
 const router = Router();
 
@@ -9,4 +10,6 @@ router.get("/fetch-brand-orders", userAuth({ ability: "fetch_brand_orders" }), f
 router.post("/get-payment-link", userAuth({ ability: "create_payment" }), createPaymentLink);
 router.post("/update-order-rating", userAuth({ ability: "update_rating_order" }), updateOrderRating);
 router.post("/update-item-rating", userAuth({ ability: "update_rating_order" }), itemRating);
+router.delete("/delete-order/:id", userAuth({ ability: "delete_user_order" }),deleteOrder);
+router.post("/send_cancel_order__email",userAuth({ ability: "send_email_cancel_order" }), sendEmailCancelOrder);
 export default router;
